9838a418a661b5735cc57b39804a620926e777dd
balineprem <48473405+balineprem@users.noreply.github.com>
2021-03-17 21:08:27 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,42 @@
+//YOUR FIREBASE LINKS
+var firebaseConfig = {
+    apiKey: "AIzaSyAuY_zhN1klllYuIXHJiQpuuuveUqPfkhc",
+    authDomain: "kwitter-e883b.firebaseapp.com",
+    databaseURL: "https://kwitter-e883b-default-rtdb.firebaseio.com",
+    projectId: "kwitter-e883b",
+    storageBucket: "kwitter-e883b.appspot.com",
+    messagingSenderId: "725952393298",
+    appId: "1:725952393298:web:3ee4477413792bff02af8b",
+    measurementId: "G-G56GJKCW9F"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("user_name")
+room_name = localStorage.getItem("room_name")
+
+function send() {
+    msg = document.getElementById("msg").value
+    firebase.database().ref(room_name).push({
+          name: user_name,
+          message: msg,
+          like: 0
+    })
+    document.getElementById("msg").value = ""
+}
+
+
+
+function getData() {
+    firebase.database().ref("/" + room_name).on('value', function (snapshot) {
+          document.getElementById("output").innerHTML = "";
+          snapshot.forEach(function (childSnapshot) {
+                childKey = childSnapshot.key;
+                childData = childSnapshot.val();
+                if (childKey != "purpose") {
+                      firebase_message_id = childKey;
+                      message_data = childData;
+                      //Start code
+                      name = message_data["name"]
+                      message = message_data["message"]
+                      like = message_data["like"]
